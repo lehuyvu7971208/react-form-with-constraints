e5e559408c1c9a4899b5f7bf271f8008aaf34c03
Tanguy Krotoff <tkrotoff@gmail.com>
2018-02-22 14:43:14 +0100

    Fix Jest console.assert
    See console.assert not throwing with v22.4.0 https://github.com/facebook/jest/issues/5634

--- packages/react-form-with-constraints/src/SetupEnzyme.ts
+++ packages/react-form-with-constraints/src/SetupEnzyme.ts
@@ -2,3 +2,7 @@ import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
 configure({adapter: new Adapter()});
+
+// FIXME See console.assert not throwing with v22.4.0 https://github.com/facebook/jest/issues/5634
+import assert from 'assert';
+console.assert = assert;
