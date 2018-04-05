0617f5ce83508b2d735068407a790b076abd9c37
Tanguy Krotoff <tkrotoff@gmail.com>
2018-04-05 14:37:19 +0200

    s WIP Group FieldFeedbacks

--- packages/react-form-with-constraints/src/FieldFeedbackWhenValid.test.tsx
+++ packages/react-form-with-constraints/src/FieldFeedbackWhenValid.test.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { shallow as _shallow } from 'enzyme';
 
 import {
-  FormWithConstraints,
-  FieldValidatedEvent, ResetEvent,
+  FormWithConstraints, Field,
+  FieldWillValidateEvent, FieldDidValidateEvent, ResetEvent,
   FieldFeedbackWhenValid, FieldFeedbackWhenValidProps, FieldFeedbackWhenValidContext
 } from './index';
 import new_FormWithConstraints from './FormWithConstraintsEnzymeFix';
@@ -31,55 +31,59 @@ test('constructor', () => {
 });
 
 test('componentWillMount() componentWillUnmount()', () => {
-  const addFieldValidatedEventListenerSpy = jest.spyOn(form_username, 'addFieldValidatedEventListener');
+  const addFieldWillValidateEventListenerSpy = jest.spyOn(form_username, 'addFieldWillValidateEventListener');
+  const addFieldDidValidateEventListenerSpy = jest.spyOn(form_username, 'addFieldDidValidateEventListener');
   const addResetEventListenerSpy = jest.spyOn(form_username, 'addResetEventListener');
-  const removeFieldValidatedEventListenerSpy = jest.spyOn(form_username, 'removeFieldValidatedEventListener');
+  const removeFieldWillValidateEventListenerSpy = jest.spyOn(form_username, 'removeFieldWillValidateEventListener');
+  const removeFieldDidValidateEventListenerSpy = jest.spyOn(form_username, 'removeFieldDidValidateEventListener');
   const removeResetEventListenerSpy = jest.spyOn(form_username, 'removeResetEventListener');
 
   const wrapper = shallow(
     <FieldFeedbackWhenValid />,
     {context: {form: form_username, fieldFeedbacks: fieldFeedbacks_username}}
   );
-  expect(addFieldValidatedEventListenerSpy).toHaveBeenCalledTimes(1);
-  expect(removeFieldValidatedEventListenerSpy).toHaveBeenCalledTimes(0);
-  expect(form_username.fieldValidatedEventEmitter.listeners.get(FieldValidatedEvent)).toHaveLength(1);
+  expect(addFieldWillValidateEventListenerSpy).toHaveBeenCalledTimes(1);
+  expect(addFieldDidValidateEventListenerSpy).toHaveBeenCalledTimes(1);
+  expect(removeFieldWillValidateEventListenerSpy).toHaveBeenCalledTimes(0);
+  expect(removeFieldDidValidateEventListenerSpy).toHaveBeenCalledTimes(0);
+  expect(form_username.fieldWillValidateEventEmitter.listeners.get(FieldWillValidateEvent)).toHaveLength(1);
+  expect(form_username.fieldDidValidateEventEmitter.listeners.get(FieldDidValidateEvent)).toHaveLength(1);
   expect(addResetEventListenerSpy).toHaveBeenCalledTimes(1);
   expect(removeResetEventListenerSpy).toHaveBeenCalledTimes(0);
   expect(form_username.resetEventEmitter.listeners.get(ResetEvent)).toHaveLength(1);
 
   wrapper.unmount();
-  expect(addFieldValidatedEventListenerSpy).toHaveBeenCalledTimes(1);
-  expect(removeFieldValidatedEventListenerSpy).toHaveBeenCalledTimes(1);
-  expect(form_username.fieldValidatedEventEmitter.listeners.get(FieldValidatedEvent)).toEqual(undefined);
+  expect(addFieldWillValidateEventListenerSpy).toHaveBeenCalledTimes(1);
+  expect(addFieldDidValidateEventListenerSpy).toHaveBeenCalledTimes(1);
+  expect(removeFieldWillValidateEventListenerSpy).toHaveBeenCalledTimes(1);
+  expect(removeFieldDidValidateEventListenerSpy).toHaveBeenCalledTimes(1);
+  expect(form_username.fieldWillValidateEventEmitter.listeners.get(FieldWillValidateEvent)).toEqual(undefined);
+  expect(form_username.fieldDidValidateEventEmitter.listeners.get(FieldDidValidateEvent)).toEqual(undefined);
   expect(addResetEventListenerSpy).toHaveBeenCalledTimes(1);
   expect(removeResetEventListenerSpy).toHaveBeenCalledTimes(1);
   expect(form_username.resetEventEmitter.listeners.get(ResetEvent)).toEqual(undefined);
 });
 
-test('fieldValidated()', async () => {
+test('fieldWillValidate() fieldDidValidate()', async () => {
   const wrapper = shallow(
     <FieldFeedbackWhenValid />,
     {context: {form: form_username, fieldFeedbacks: fieldFeedbacks_username}}
   );
   const fieldFeedbackWhenValid = wrapper.instance() as FieldFeedbackWhenValid;
 
-  let fieldValidationsPromise: any = Promise.resolve({isValid: () => true});
-  form_username.emitFieldValidatedEvent({name: 'username'} as any, fieldValidationsPromise);
   expect(fieldFeedbackWhenValid.state.fieldIsValid).toEqual(undefined);
-  await fieldValidationsPromise;
+
+  let noReturn = await form_username.emitFieldWillValidateEvent('username');
+  expect(noReturn).toEqual([undefined]);
+  expect(fieldFeedbackWhenValid.state.fieldIsValid).toEqual(undefined);
+
+  noReturn = await form_username.emitFieldDidValidateEvent(new Field('username'));
+  expect(noReturn).toEqual([undefined]);
   expect(fieldFeedbackWhenValid.state.fieldIsValid).toEqual(true);
 
-  fieldValidationsPromise = Promise.resolve({isValid: () => false});
-  form_username.emitFieldValidatedEvent({name: 'username'} as any, fieldValidationsPromise);
-  expect(fieldFeedbackWhenValid.state.fieldIsValid).toEqual(undefined);
-  await fieldValidationsPromise;
-  expect(fieldFeedbackWhenValid.state.fieldIsValid).toEqual(false);
-
-  fieldValidationsPromise = Promise.resolve({isValid: () => true});
-  form_username.emitFieldValidatedEvent({name: 'unknown'} as any, fieldValidationsPromise);
-  expect(fieldFeedbackWhenValid.state.fieldIsValid).toEqual(false);
-  await fieldValidationsPromise;
-  expect(fieldFeedbackWhenValid.state.fieldIsValid).toEqual(false);
+  noReturn = await form_username.emitFieldWillValidateEvent('unknown');
+  expect(noReturn).toEqual([undefined]);
+  expect(fieldFeedbackWhenValid.state.fieldIsValid).toEqual(true); // Untouched
 });
 
 test('reset()', async () => {
@@ -89,13 +93,11 @@ test('reset()', async () => {
   );
   const fieldFeedbackWhenValid = wrapper.instance() as FieldFeedbackWhenValid;
 
-  const fieldValidationsPromise: any = Promise.resolve({isValid: () => true});
-  form_username.emitFieldValidatedEvent({name: 'username'} as any, fieldValidationsPromise);
-  expect(fieldFeedbackWhenValid.state.fieldIsValid).toEqual(undefined);
-  await fieldValidationsPromise;
+  await form_username.emitFieldDidValidateEvent(new Field('username'));
   expect(fieldFeedbackWhenValid.state.fieldIsValid).toEqual(true);
 
-  form_username.emitResetEvent();
+  const noReturn = await form_username.emitResetEvent();
+  expect(noReturn).toEqual([undefined]);
   expect(fieldFeedbackWhenValid.state.fieldIsValid).toEqual(undefined);
 });
 
